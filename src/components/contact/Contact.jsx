4a11bc15cf7160b597068ef5d5bc4e112f0a438d
Alexander Philippopoulos <161390808+ajax815@users.noreply.github.com>
2025-03-29 13:18:45 -0400

    Created contact page

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+import linkedin_icon from '../../assets/icons8-linkedin.svg'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        <div className="contact-title">
+            <h1>Contact Me</h1>
+            <img src={theme_pattern} alt=""/>
+        </div>
+        <div className="contact-section">
+            <div className="contact-left">
+                <h1>Let's Talk</h1>
+                <p>I'm currently available to help with any projects, so feel free to reach out to me.</p>
+                <div className="contact-detail">
+                    <img src={mail_icon} alt="" /> <p>alexanderphilippopoulos@gmail.com</p>
+                </div>
+                <div className="contact-detail">
+                    <img src={call_icon} alt="" /> <p>647-975-2248</p>
+                </div>
+                <div className="contact-detail">
+                    <img src={linkedin_icon} alt="" /> <p>My LinkedIn</p>
+                </div>
+            </div>
+            <form className="contact-right">
+                <label htmlFor="">Your Name</label>
+                <input type="text"placeholder='Enter your name' name='name' />
+                <label htmlFor="">Your Email</label>
+                <input type="email"placeholder='Enter your email' name='email' />
+                <label htmlFor="">Write your message here</label>
+                <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
+                <button type='submit' className="contact-submit">Submit now</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
