bee31ccdcdc66bca2107774cff127adc5c20ba1c
Alexander Philippopoulos <161390808+ajax815@users.noreply.github.com>
2025-03-31 23:27:36 -0400

    Updated hero page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -13,9 +13,9 @@ const Hero = () => {
   }, [])
   return (
     <div id='home' className='hero'>
-      <h1 data-aos="fade-up">Welcome!</h1>
+      <h1 data-aos="fade-up"><span>Welcome!</span></h1>
         <img src={profile_img} alt="" data-aos="fade-up" data-aos-delay="300"/>
-        <p>Hello! My name is <span>Alexander Philippopoulos</span>. I'm a <br></br>
+        <p data-aos="fade-up" data-aos-delay="600">Hello! My name is <span>Alexander Philippopoulos</span>. I'm a <br></br>
           <div className="type-animation">
              <TypeAnimation
               sequence={[
@@ -33,11 +33,8 @@ const Hero = () => {
           </div>
         <br></br> based in the <span>Greater Toronto Area</span>.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect with me</div>
-            <div className="hero-resume">My resume</div>
-        </div>
-        <div className="video-section">
-          <VideoComponent />
+            <div className="hero-connect" data-aos="fade-right" data-aos-delay="900">Connect with me</div>
+            <div className="hero-resume" data-aos="fade-left" data-aos-delay="900">My resume</div>
         </div>
     </div>
   )
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -7,15 +7,17 @@ const Navbar = () => {
 
   const [menu, setMenu] = useState("home");
 
+  const anchorOffset = 100;
+
   return (
     <div id='navbar' className='navbar'>
         <img src={logo} alt="" />
         <ul className="nav-menu">
-            <li><AnchorLink className='anchor-link' offset={50} href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink></li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink></li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink></li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#projects'><p onClick={()=>setMenu("projects")}>Projects</p></AnchorLink></li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={anchorOffset} href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={anchorOffset} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={anchorOffset} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={anchorOffset} href='#projects'><p onClick={()=>setMenu("projects")}>Projects</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={anchorOffset} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink></li>
         </ul>
         <div className="nav-connect">Connect With Me</div>
     </div>
