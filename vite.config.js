e14f71ee4b61935a6467775caab6c2289729d526
Alexander Philippopoulos <161390808+ajax815@users.noreply.github.com>
2025-04-01 15:48:27 -0400

    Prevented horzintal scrolling

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This allows external devices to access the server
+    port: 3000,        // Optional: change the port if you want to use a different one
+  }
 })
