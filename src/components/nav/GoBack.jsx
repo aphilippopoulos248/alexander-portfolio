27a3e2fe94bd58ecf0212e94a2f8b88bb6a57dd2
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2025-04-22 13:17:00 -0400

    Updated navbars

--- src/components/nav/GoBack.jsx
+++ src/components/nav/GoBack.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 import './GoBack.scss'
 import logo from '../../assets/ap-logo.png'
 import { useNavigate } from 'react-router-dom'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import menu_open from "../../assets/menu_open.svg"
-import menu_close from "../../assets/menu_close.svg"
-import linkedin_icon from "../../assets/icons8-linkedin.svg"
-import github_icon from "../../assets/icons8-github-48.svg"
-import mail_icon from "../../assets/gmail.svg"
+import arrow_icon from "../../assets/arrow_icon.svg"
 
 const GoBack = () => {
   const navigate = useNavigate();
@@ -21,11 +16,11 @@ const GoBack = () => {
       <div className="nav-logo">
         <img src={logo} alt="" />
       </div>
-      <ul className="nav-menu">
-          <img alt="" className='nav-mob-close' />
-          <div className="go-back-button">
-            <li><p onClick={handleGoBack}>Go Back</p></li>
-          </div>
+      <ul className="go-back-menu">
+        <li className="go-back-button" onClick={handleGoBack}>
+          <img src={arrow_icon} alt="Arrow icon" />
+          <p>Go Back</p>
+        </li>
       </ul>
     </div>
   )
