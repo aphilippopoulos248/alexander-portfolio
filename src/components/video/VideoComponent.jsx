7d9ed83a4bf8afe3ef008ee92005cd449054685f
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2025-05-03 00:03:15 -0400

    Updated video component

--- src/components/video/VideoComponent.jsx
+++ src/components/video/VideoComponent.jsx
@@ -5,7 +5,7 @@ const VideoComponent = () => {
   return (
     <div className='video-component'>
       <video width="100%" controls>
-        <source src="/videos/FullAnimationV4.mp4" type="video/mp4" />
+        <source src="/videos/FullAnimationV5.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
