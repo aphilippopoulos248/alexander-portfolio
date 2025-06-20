45a5a39adfcd563e9a3761fffce84132254dece9
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2025-06-19 23:57:43 -0400

    Updated my work and projects page

--- src/data/mywork_data.js
+++ src/data/mywork_data.js
@@ -2,8 +2,8 @@ import project1_img from '../assets/SunnyLand.png'
 import project2_img from '../assets/GrimwoodManor.png'
 import project3_img from '../assets/jurassic-world-hub.png'
 import project4_img from '../assets/EventHorizon.jpg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project5_img from '../assets/AnimalSimulator.png'
+import project6_img from '../assets/blinky-icon.png'
 
 const mywork_data = [
     {
@@ -29,6 +29,18 @@ const mywork_data = [
         w_name:"Event Horizon",
         w_img:project4_img,
         w_link:"https://github.com/JoshLanesmith/Events_MobileApp"
+    },
+    {
+        w_no:5,
+        w_name:"Animal Simulator",
+        w_img:project5_img,
+        w_link:"https://aphilippopoulos248.itch.io/animal-simulator"
+    },
+    {
+        w_no:6,
+        w_name:"Blinky",
+        w_img:project6_img,
+        w_link:"https://github.com/aphilippopoulos248/Blinky-Bot"
     },
 ]
  
