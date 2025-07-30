33a3b162083504e930d9435a00956afeb98169de
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2025-07-29 22:49:31 -0400

    Updated AI page

--- src/data/ai_data.js
+++ src/data/ai_data.js
@@ -1,11 +1,18 @@
 import project1_img from '../assets/blinky-icon.png'
+import project2_img from '../assets/bob.png'
 
 const AI_Data = [
     {
         a_no:1,
-        a_name:"Blinky",
+        a_name:"Blinky the Discord Bot",
         a_img:project1_img,
         a_link:"https://github.com/aphilippopoulos248/Blinky-Bot"
+    },
+    {
+        a_no:2,
+        a_name:"Bob the MetaHuman",
+        a_img:project2_img,
+        a_link:"https://github.com/aphilippopoulos248/MetaHuman-code"
     }
 ]
  
